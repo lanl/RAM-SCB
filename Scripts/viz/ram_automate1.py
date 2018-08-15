5fdf93711e45e4eec0cf6b48bb55c255827d1214
Steve Morley <smorley@lanl.gov>
2018-08-15 13:56:16 -0600

    viz: new PolyData representation of streamline tracing source object

--- Scripts/viz/ram_automate1.py
+++ Scripts/viz/ram_automate1.py
@@ -7,6 +7,7 @@
 import os, sys
 import numpy as np
 import spacepy.datamodel as dm
+from makeCustomSource import gen_sphere
 
 #========================================================================================================
 def read_config():
@@ -133,64 +134,64 @@ def gen_vts(fileName):
 	with open('vts_files/' + fileName[:-3] + '_field.vts', 'w') as fh:
 		fh.write(to_write)
 #=================================================================================================
-def gen_sphere():
-	'''Generating the sphere source for streamlines as sphere.vts in vts_files directory'''
-	n = 1 #how much denser points on sphere are, compared to the magnetic field
-	#----------------------------generate the points describing the sphere--------------------------
-	theta = np.linspace(0, 2*np.pi, 873*n).tolist() #latitudes
-	dtor = np.pi/180.0
-	delta = 90.0/(253.0*n)
-
-	#generate 1st hemisphere
-	L_lo = 1.0/np.sin(delta*dtor) #90/(253*n)  {0}
-	L_hi = 1.0/np.sin(90*dtor)  #{90}
-	Lvalues = np.linspace(L_lo,L_hi,253.0*n*n).tolist()
-	phi = map(lambda x: np.arcsin(1.0/x),Lvalues)
-	#generate 2nd hemisphere
-	L_lo = 1.0/np.cos((90 + delta)*dtor)  #{90}
-	L_hi = 1.0/np.cos((-180 + delta)*dtor) #90/(253*n) - 180  {180}
-	Lvalues2 = np.linspace(L_lo,L_hi,252*n*n).tolist()
-	phi2 = map(lambda x: np.arccos(1.0/x),Lvalues2)
-	phi2 = map(lambda x: x+(np.pi/2), phi2)
-
-	#combine the two hemispheres
-	phi.extend(phi2)
-
-	r = 1
-	points = []
-	for t in theta:
-		for p in phi:
-			points.append((r*np.cos(t)*np.sin(p), r*np.sin(t)*np.sin(p), r*np.cos(p)))
-	#----------------------------generate the sphere.vts file--------------------------
-	to_write = ''
-
-	to_write += '<?xml version="1.0"?>\n'  #1
-	to_write += '<VTKFile type="StructuredGrid" version="0.1" byte_order="LittleEndian">\n' #2
-	to_write += '\t<StructuredGrid WholeExtent="0 {} 0 {} 0 {}">\n'.format(97*n-1, 45*n-1, 101*n-1) #3
-	to_write += '\t\t<Piece Extent="0 {} 0 {} 0 {}">\n'.format(97*n-1, 45*n-1, 101*n-1) #4
-
-	to_write += '\t\t\t<PointData Scalars="dummy">\n' #5
-	to_write += '\t\t\t\t<DataArray type="Int32" Name="dummy" NumberOfComponents="1" format="ascii">\n' #6
-	for i in range(97*45*101*(n**3)):#440865
-		to_write += '1 ' #7
-	to_write += '\n\t\t\t\t</DataArray>\n' #8
-	to_write += '\t\t\t</PointData>\n' #9
-	to_write += '\t\t\t<CellData>\n'
-	to_write += '\t\t\t</CellData>\n'
-	to_write += '\t\t\t<Points>\n'
-	to_write += '\t\t\t\t<DataArray type="Float32" NumberOfComponents="3" format="ascii">\n'
-	for p in points:
-		to_write += '\t\t\t\t\t' + str(p[0]) + ' ' + str(p[1]) + ' ' + str(p[2]) + '\n'
-	to_write += '\t\t\t\t</DataArray>\n'
-	to_write += '\t\t\t</Points>\n'
-
-	to_write += '\t\t</Piece>\n'
-	to_write += '\t</StructuredGrid>\n'
-	to_write += '</VTKFile>\n'
-
-	with open('vts_files/sphere.vts', 'w') as fh:
-		fh.write(to_write)
-
+#def gen_sphere():
+#	'''Generating the sphere source for streamlines as sphere.vts in vts_files directory'''
+#	n = 1 #how much denser points on sphere are, compared to the magnetic field
+#	#----------------------------generate the points describing the sphere--------------------------
+#	theta = np.linspace(0, 2*np.pi, 873*n).tolist() #latitudes
+#	dtor = np.pi/180.0
+#	delta = 90.0/(253.0*n)
+#
+#	#generate 1st hemisphere
+#	L_lo = 1.0/np.sin(delta*dtor) #90/(253*n)  {0}
+#	L_hi = 1.0/np.sin(90*dtor)  #{90}
+#	Lvalues = np.linspace(L_lo,L_hi,253.0*n*n).tolist()
+#	phi = map(lambda x: np.arcsin(1.0/x),Lvalues)
+#	#generate 2nd hemisphere
+#	L_lo = 1.0/np.cos((90 + delta)*dtor)  #{90}
+#	L_hi = 1.0/np.cos((-180 + delta)*dtor) #90/(253*n) - 180  {180}
+#	Lvalues2 = np.linspace(L_lo,L_hi,252*n*n).tolist()
+#	phi2 = map(lambda x: np.arccos(1.0/x),Lvalues2)
+#	phi2 = map(lambda x: x+(np.pi/2), phi2)
+#
+#	#combine the two hemispheres
+#	phi.extend(phi2)
+#
+#	r = 1
+#	points = []
+#	for t in theta:
+#		for p in phi:
+#			points.append((r*np.cos(t)*np.sin(p), r*np.sin(t)*np.sin(p), r*np.cos(p)))
+#	#----------------------------generate the sphere.vts file--------------------------
+#	to_write = ''
+#
+#	to_write += '<?xml version="1.0"?>\n'  #1
+#	to_write += '<VTKFile type="StructuredGrid" version="0.1" byte_order="LittleEndian">\n' #2
+#	to_write += '\t<StructuredGrid WholeExtent="0 {} 0 {} 0 {}">\n'.format(97*n-1, 45*n-1, 101*n-1) #3
+#	to_write += '\t\t<Piece Extent="0 {} 0 {} 0 {}">\n'.format(97*n-1, 45*n-1, 101*n-1) #4
+#
+#	to_write += '\t\t\t<PointData Scalars="dummy">\n' #5
+#	to_write += '\t\t\t\t<DataArray type="Int32" Name="dummy" NumberOfComponents="1" format="ascii">\n' #6
+#	for i in range(97*45*101*(n**3)):#440865
+#		to_write += '1 ' #7
+#	to_write += '\n\t\t\t\t</DataArray>\n' #8
+#	to_write += '\t\t\t</PointData>\n' #9
+#	to_write += '\t\t\t<CellData>\n'
+#	to_write += '\t\t\t</CellData>\n'
+#	to_write += '\t\t\t<Points>\n'
+#	to_write += '\t\t\t\t<DataArray type="Float32" NumberOfComponents="3" format="ascii">\n'
+#	for p in points:
+#		to_write += '\t\t\t\t\t' + str(p[0]) + ' ' + str(p[1]) + ' ' + str(p[2]) + '\n'
+#	to_write += '\t\t\t\t</DataArray>\n'
+#	to_write += '\t\t\t</Points>\n'
+#
+#	to_write += '\t\t</Piece>\n'
+#	to_write += '\t</StructuredGrid>\n'
+#	to_write += '</VTKFile>\n'
+#
+#	with open('vts_files/sphere.vts', 'w') as fh:
+#		fh.write(to_write)
+#
 #=================================================================================================
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
