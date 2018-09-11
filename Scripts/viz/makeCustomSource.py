ecca0e698806b7d32bc58fcfc4e55cf654725687
Steve Morley <smorley@lanl.gov>
2018-09-11 14:05:09 -0600

    viz/makeCustomSource: updated to calculate connectivity and cell info

--- Scripts/viz/makeCustomSource.py
+++ Scripts/viz/makeCustomSource.py
@@ -11,31 +11,59 @@ import spacepy.datamodel as dm
 
 #=================================================================================================
 def gen_sphere():
-    '''Generating the sphere source for streamlines as sphere.vts in vts_files directory'''
+    '''Generating a spherical source for streamlines as sphere.vts in vts_files directory'''
     #----------------------------generate the points describing the sphere--------------------------
-    xyz = pointsGen(r=1.1, minL=2.0, maxL=7.0, dL=0.25)
+    xyz, nLons, nLats = pointsGenSphere(r=1.1, minL=2.0, maxL=7.0, dL=0.25)
 
     #----------------------------generate the sphere.vtx file--------------------------
-    to_write = xmlPolyGen(xyz)
+    to_write = xmlPolyGen(xyz, dim1=nLons, dim2=nLats)
     with open('vts_files/sphere.vtp', 'wb') as fh:
         fh.write(to_write)
 
-def pointsGen(r=1.1, minL=2.0, maxL=7.0, dL=0.25):
+
+def gen_disc():
+    '''Generating a disc source for streamlines as disc.vts in vts_files directory'''
+    #----------------------------generate the points describing the sphere--------------------------
+    xyz, nLons, nL = pointsGenDisc(minL=4.0, maxL=6.5, dL=0.25)
+
+    #----------------------------generate the sphere.vtx file--------------------------
+    to_write = xmlPolyGen(xyz, dim1=nLons, dim2=nL)
+    with open('vts_files/disc.vtp', 'wb') as fh:
+        fh.write(to_write)
+
+
+def pointsGenSphere(r=1.1, minL=2.0, maxL=7.0, dL=0.25):
     points = []
     Ls = np.arange(minL, maxL, dL)
     Lats = np.arccos(np.sqrt(1./Ls)) #N.Hem latitudes in radians
-    Lats = np.concatenate([-1*Lats[::-1], Lats]) #add S.Hem
+    #Lats = np.concatenate([-1*Lats[::-1], Lats]) #add S.Hem
     Colats = (np.pi/2.)-Lats #Colatitudes in radians
     Lons = np.deg2rad(np.arange(0,360,15))
     npts = len(Lats)*len(Lons)
     xyz = np.zeros([npts,3])
-    for idx, LatLon in enumerate(itertools.product(Lons,Colats)):
-        xyz[idx,0] = r*np.cos(LatLon[0])*np.sin(LatLon[1])
-        xyz[idx,1] = r*np.sin(LatLon[0])*np.sin(LatLon[1])
-        xyz[idx,2] = r*np.cos(LatLon[1])
-    return xyz
+    for idx, LonLat in enumerate(itertools.product(Lons,Colats)):
+        xyz[idx,0] = r*np.cos(LonLat[0])*np.sin(LonLat[1])
+        xyz[idx,1] = r*np.sin(LonLat[0])*np.sin(LonLat[1])
+        xyz[idx,2] = r*np.cos(LonLat[1])
+    return xyz, len(Lons), len(Lats)
 
-def xmlPolyGen(xyz):
+
+def pointsGenDisc(minL=2.0, maxL=7.0, dL=0.25):
+    points = []
+    Ls = np.arange(minL, maxL, dL)
+    Lats = np.zeros(len(Ls))
+    Colats = (np.pi/2.)-Lats #Colatitudes in radians
+    Lons = np.deg2rad(np.arange(0,360,30))
+    npts = len(Lats)*len(Lons)
+    xyz = np.zeros([npts,3])
+    for idx, LonL in enumerate(itertools.product(Lons,Ls)):
+        xyz[idx,0] = LonL[1]*np.cos(LonL[0])
+        xyz[idx,1] = LonL[1]*np.sin(LonL[0])
+        xyz[idx,2] = 0.0
+    return xyz, len(Lons), len(Ls)
+
+
+def xmlPolyGen(xyz, dim1, dim2):
     import lxml.etree
     npts = len(xyz)
 
@@ -70,7 +98,23 @@ def xmlPolyGen(xyz):
     piece.append(verts)
     for partname in ['Lines', 'Strips', 'Polys']:
         dac = lxml.etree.Element('DataArray', type='Int64', Name='connectivity', RangeMax='{}'.format(npts), RangeMin='0')
-        dao = lxml.etree.Element('DataArray', type='Int64', Name='offsets', RangeMin='{}'.format(npts), RangeMax='{}'.format(npts))
+        dao = lxml.etree.Element('DataArray', type='Int64', Name='offsets', RangeMin='{}'.format(0), RangeMax='{}'.format(npts))
+        if partname is 'Polys':
+            polyconn = getPolyVertOrder(xyz, dim1, dim2)
+            nconn = len(polyconn)
+            npolys = dim1*(dim2-1)
+            for i in range(nconn//4)[::-1]:
+                polyconn.insert((i+1)*4, '\n')
+            connstr = ' '.join(polyconn)
+            #offsets are 4, 8, 12, ...
+            polyoffset = ['{}'.format(4*(n+1)) for n in range(nconn//4)]
+            for i in range(nconn//4)[::-1]:
+                polyoffset.insert((i+1)*4, '\n')
+            offstr = ' '.join(polyoffset)
+            dac.text = '\n' + connstr
+            dao.text = '\n' + offstr
+            dao.set('RangeMax', '{}'.format(nconn))
+            piece.set('NumberOfPolys', '{}'.format(npolys))
         part = lxml.etree.Element(partname)
         part.append(dac)
         part.append(dao)
@@ -80,7 +124,28 @@ def xmlPolyGen(xyz):
     out = lxml.etree.tostring(fulltree, xml_declaration=False, pretty_print=True)
     return out
 
+def getPolyVertOrder(xyz, dim1, dim2):
+    # x, y, z are calculated using itertools.product
+    # given (for disc) Lon, L, the loop order is all L for each Lon, iterating over Lon
+    # i.e. (Lon1, L1), (Lon1, L2), (Lon1, L3), ..., (Lon2, L1), (Lon2, L2), ...
+    cells = []
+    for idx1, idx2 in itertools.product(range(dim1-1), range(dim2-1)):
+        vert1 = idx1*dim2+idx2
+        vert2 = vert1+1
+        vert3 = (idx1+1)*dim2+idx2+1
+        vert4 = (idx1+1)*dim2+idx2
+        cells.append([vert1, vert2, vert3, vert4])
+    #add cells that wrap in Longitude
+    for idx in range(dim2-1):
+        vert1 = idx
+        vert2 = idx+1
+        vert3 = len(xyz)-dim2+idx+1
+        vert4 = len(xyz)-dim2+idx
+        cells.append([vert1, vert2, vert3, vert4])
+    flat_cells = [str(item) for sublist in cells for item in sublist]
+    return flat_cells
 
 #=================================================================================================
 if __name__ == '__main__':
     gen_sphere() #generates sphere.vtp in vts_files directory
+    gen_disc()
