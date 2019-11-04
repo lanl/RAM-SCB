ab72b31ff269c8a1a59d5f1f79f3985cf54977eb
Steve Morley <steven.k.morley@gmail.com>
2019-11-03 23:30:11 -0700

    updates to CCMC setup script
    Add GSL include directories to C compilation

--- ccmcSetup.sh
+++ ccmcSetup.sh
@@ -7,7 +7,7 @@
 module purge
 LD_LIBRARY_PATH=
 module load gcc-9.2.0
-#TODO: add module load for netcdf here
+module load netcdf-c-4.7.2/gcc-9.2.0
 module load netcdf-fortran-4.5.2/gcc-9.2.0
 module load openmpi-3.1.4/gcc-9.2.0
 conda activate python37
@@ -15,10 +15,12 @@ conda activate python37
 PKG_CONFIG_PATH=/opt/gnu/gsl-2.5/lib/pkgconfig/
 LD_LIBRARY_PATH=/opt/gnu/gsl-2.5/lib:$LD_LIBRARY_PATH
 NCDF=/act/netcdf-fortran-4.5.2/gcc-9.2.0
+GSL=/opt/gnu/gsl-2.5
 
-./Config.pl -install -compiler=gfortran -mpi=openmpi -ncdf=${NCDF} -gsl=/opt/gnu/gsl-2.5 -openmp
+./Config.pl -install -compiler=gfortran -mpi=openmpi -ncdf=${NCDF} -gsl=${GSL} -openmp
 #now fix the GSL include and link stuff in the make system
-sed -i 's/^\(FLAGC.*\)/SEARCH_C = `pkg-config --cflags --libs gsl`\n\1/' Makefile.conf
+GSL_FLAGS=`pkg-config --cflags --libs gsl`
+sed "s|^\(FLAGC.*\)|SEARCH_C = -I$GSL/include\\nFLAGC_EXTRA = $GSL_FLAGS\\n\\1|" Makefile.conf
 
 #Then compile
-Make
+make
