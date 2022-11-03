697b7d3f12ddec910481ac39d8d325bc5c515bfb
Steve Morley <steven.k.morley@gmail.com>
2022-11-03 16:16:23 -0600

    summaryPlots: ensure parser robustness and time handling

--- Scripts/summaryPlots.py
+++ Scripts/summaryPlots.py
@@ -4,10 +4,12 @@ from argparse import ArgumentParser
 import os
 import subprocess
 import sys
+import warnings
 
 import matplotlib.pyplot as plt
 from spacepy.pybats import ram
 import spacepy.plot as splot
+import spacepy.time as spt
 
 
 @contextmanager
@@ -56,14 +58,15 @@ def parserSetup():
     return parser
 
 
-def plotDst(options):
-    """Make default plot of RAM simulated Dst
-    """
+def load_logs(options):
     logcands = sorted(glob.glob(os.path.join(options.rundir,
                                              'output_ram',
                                              'log*log')))
     outfile = os.path.join(options.rundir, 'output_ram',
                            'log_combined.log')
+    if outfile in logcands:
+        oidx = logcands.index(outfile)
+        _ = logcands.pop(oidx)
     if len(logcands) > 1:
         subprocess.run(["python", "CatLog.py",
                         f"-o={outfile}",
@@ -72,10 +75,36 @@ def plotDst(options):
     else:
         uselog = logcands[0]
     log = ram.LogFile(uselog)
+    return log
 
-    # put figure together here
+
+def plotDst(log, options):
+    """Make default plot of RAM simulated Dst
+    """
     fig = plt.figure(tight_layout=True, figsize=(8, 4.5))
-    log.add_dst_quicklook(target=fig, showBiot=False)
+    fig, ax = log.add_dst_quicklook(target=fig, showBiot=False)
+    tstart = log['time'][0]
+    tstart_ISO = spt.Ticktock(tstart).ISO[0]
+    tend = log['time'][-1]
+    tend_ISO = spt.Ticktock(tend).ISO[0]
+    if options.startTime:
+        opt_tst = spt.Ticktock(options.startTime).UTC[0]
+        # start needs to be in valid range
+        if (opt_tst > tstart) and (opt_tst < tend):
+            tstart = opt_tst
+        else:
+            warnings.warn(f'Supplied start time ({options.startTime}) ' +
+                          f'is out of valid range. Using {tstart_ISO}.')
+    if options.endTime:
+        opt_ten = spt.Ticktock(options.endTime).UTC[0]
+        # end needs to be in valid range
+        if (opt_ten > tstart) and (opt_ten < tend):
+            tend = opt_ten
+        else:
+            warnings.warn(f'Supplied end time ({options.endTime}) ' +
+                          f'is out of valid range. Using {tend_ISO}.')
+
+    ax.set_xlim([tstart, tend])
     # output figure to either rundir/plots or custom
     runname = os.path.split(options.rundir)[-1]
     figname = 'ram_dst_{}.png'.format(runname)
@@ -105,7 +134,10 @@ if __name__ == "__main__":
         logcand = os.path.join(in_args.rundir, 'output_ram', 'log*log')
         if len(glob.glob(logcand)) > 0:
             splot.style('default')
-            plotDst(in_args)
+            uselog = load_logs(in_args)
+            plotDst(uselog, in_args)
+    else:
+        bailflag = True
 
     if bailflag:
         errmsg = 'Run directory {} does not exist '.format(in_args.rundir)
